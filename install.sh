dfbf6e7f015f9a5b7d5e79f406fe981317ed4802
vavikast <30380259+vavikast@users.noreply.github.com>
2020-12-16 13:52:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ echo "net.ipv4.ip_forward = 1">> /etc/sysctl.conf
 sysctl -p
 yum makecache fast
 yum install strongswan openvpn zip curl wget
+rpm -ivh flexgw-1.1.0-1.el7.centos.x86_64.rpm
 \cp -fv /usr/local/flexgw/rc/strongswan.conf /etc/strongswan/strongswan.confconf
 \cp -fv /usr/local/flexgw/rc/openvpn.conf /etc/openvpn/server/server.conf
 sed -i  's/load/#load/g' /etc/strongswan/strongswan.d/charon/dhcp.conf
